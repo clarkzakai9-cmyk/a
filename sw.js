7199066fbe02c712e5781b09008e1fa7ffdc82b3
clarkzakai9-cmyk <clarkzakai9@gmail.com>
2026-01-21 17:44:35 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "llama-offline-v1";
+const ASSETS_TO_CACHE = [
+  "./index.html",
+  "./"
+];
+
+// Cache the website structure on install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Serve the website from cache if offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
